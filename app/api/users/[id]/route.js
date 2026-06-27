cedd23441d8c5108cb7262b491259908d036e3db
Suvodip <128118374+suvodip35@users.noreply.github.com>
2026-06-27 09:25:13 +0000

    Fix API route imports for build

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -1,5 +1,5 @@
-import dbConnect from '@/lib/mongodb';
-import User from '@/models/User';
+import dbConnect from '../../../../lib/mongodb';
+import User from '../../../../models/User';
 import { Types } from 'mongoose';
 
 export async function GET(request, { params }) {
